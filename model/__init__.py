7bd4ed414a241a27e97699b7ca330da202d664c7
Dan Radez <dan@radez.net>
2024-03-05 14:43:13 -0500

    refactor model to make imports easier

--- model/__init__.py
+++ model/__init__.py
@@ -1,21 +1,4 @@
-import json
-from sqlalchemy.orm import declarative_base
-Base = declarative_base()
-
-
-class MetaBase():
-    default_data = None
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    @property
-    def data(self):
-        return json.loads(self.meta) if self.meta else self.default_data
-
-    def store_data(self, data):
-        self.meta = json.dumps(data)
-
-    @property
-    def keys(self):
-        return self.data.keys()
+from model.base import Base, MetaBase
+from model.code import Code
+from model.notification import Notification
+from model.user import User
